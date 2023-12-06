0e4252ab8a58f22a77915f02590b12a2dcb906a7
raphael marty <marty@ZenbookR>
2023-12-06 22:27:10 +0100

    Projet en groupe Le Labyrinthe

--- Projet_Labyrinthe/src/projet_labyrinthe/Peon.java
+++ Projet_Labyrinthe/src/projet_labyrinthe/Peon.java
@@ -9,12 +9,34 @@ package projet_labyrinthe;
  * @author lukha
  */
 public class Peon {
-    Player associe;
-    String Color;
+    private String couleur; // couleur du pion (blanc, noir)
+    private int position;   // position actuelle du pion sur le plateau
 
-    public Peon(Player user, String Color) {
-        this.associe = user;
-        this.Color = Color;
+    public Peon(String couleur, int position) {
+        this.couleur = couleur;
+        this.position = position;
     }
-    
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void deplacer(int decalage) {
+        // vérifier que le déplacement est valide
+        if (decalage >= 0 && decalage <= 5) {
+            this.position += decalage;
+        } else {
+            System.out.println("Déplacement non valide");
+        }
+    }
 }
+    
+
