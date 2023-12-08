1d7f9ebb62bd611498f8e9cab76646fdfaaa9e55
owenh <owen.hayet@epfedu.fr>
2023-12-08 08:33:42 +0100

    classe CaseGraphique V-001

--- Projet_Labyrinthe/src/projet_labyrinthe/CaseGraphique.java
+++ Projet_Labyrinthe/src/projet_labyrinthe/CaseGraphique.java
@@ -14,42 +14,15 @@ import javax.swing.JButton;
  * @author lukha
  */
 public class CaseGraphique extends JButton{
-    Case caseassocie;
-    ImageIcon araignee = new ImageIcon(getClass().getResource("/Img/Cases/araignee.png"));
-    ImageIcon bague = new ImageIcon(getClass().getResource("/Img/Cases/bague.png"));
-    ImageIcon bourse = new ImageIcon(getClass().getResource("/Img/Cases/bourse.png"));
-    ImageIcon carteTresor = new ImageIcon(getClass().getResource("/Img/Cases/CarteTresor.png"));
-    ImageIcon chandelier = new ImageIcon(getClass().getResource("/Img/Cases/chandelier.png"));
-    ImageIcon chauvesouris = new ImageIcon(getClass().getResource("/Img/Cases/chauvesouris.png"));
-    ImageIcon chouette = new ImageIcon(getClass().getResource("/Img/Cases/chouette.png"));
-    ImageIcon cle = new ImageIcon(getClass().getResource("/Img/Cases/cle.png"));
-    ImageIcon couronne = new ImageIcon(getClass().getResource("/Img/Cases/couronne.png"));
-    ImageIcon crane = new ImageIcon(getClass().getResource("/Img/Cases/crane.png"));
-    ImageIcon departB = new ImageIcon(getClass().getResource("/Img/Cases/departB.png"));
-    ImageIcon departJ = new ImageIcon(getClass().getResource("/Img/Cases/departJ.png"));
-    ImageIcon departR = new ImageIcon(getClass().getResource("/Img/Cases/departR.png"));
-    ImageIcon departV = new ImageIcon(getClass().getResource("/Img/Cases/departV.png"));
-    ImageIcon dragon = new ImageIcon(getClass().getResource("/Img/Cases/dragon.png"));
-    ImageIcon epee = new ImageIcon(getClass().getResource("/Img/Cases/epee.png"));
-    ImageIcon fantome = new ImageIcon(getClass().getResource("/Img/Cases/fantome.png"));
-    ImageIcon fee = new ImageIcon(getClass().getResource("/Img/Cases/fee.png"));
-    ImageIcon genie = new ImageIcon(getClass().getResource("/Img/Cases/genie.png"));
-    ImageIcon gobelin = new ImageIcon(getClass().getResource("/Img/Cases/gobelin.png"));
-    ImageIcon heaume = new ImageIcon(getClass().getResource("/Img/Cases/heaume.png"));
-    ImageIcon lezard = new ImageIcon(getClass().getResource("/Img/Cases/lezard.png"));
-    ImageIcon livre = new ImageIcon(getClass().getResource("/Img/Cases/livre.png"));
-    ImageIcon papillon = new ImageIcon(getClass().getResource("/Img/Cases/papillon.png"));
-    ImageIcon rat = new ImageIcon(getClass().getResource("/Img/Cases/rat.png"));
-    ImageIcon saphir = new ImageIcon(getClass().getResource("/Img/Cases/saphir.png"));
-    ImageIcon scarabee = new ImageIcon(getClass().getResource("/Img/Cases/scarabee.png"));
-    ImageIcon tresor = new ImageIcon(getClass().getResource("/Img/Cases/tresor.png"));
-    ImageIcon tuile1 = new ImageIcon(getClass().getResource("/Img/Cases/tuile1.png"));
-    ImageIcon tuile2 = new ImageIcon(getClass().getResource("/Img/Cases/tuile2.png"));
-    ImageIcon blank = new ImageIcon(getClass().getResource("/Img/Cases/placeHolder.png"));
+    Case CaseGrapheAssocie;
+    int nblignes;
+    int nbcolonnes;
 
-    public CaseGraphique(Case caseassocie) {
-        this.caseassocie = caseassocie;
-    }
+    public CaseGraphique(Case CaseGrapheAssocie, int nblignes, int nbcolonnes) {
+        this.CaseGrapheAssocie = CaseGrapheAssocie;
+        this.nblignes = nblignes;
+        this.nbcolonnes = nbcolonnes;
+    }   
 
     /**
      * Modifie la taille d'une image et la renvoie sous forme d'icone
@@ -76,7 +49,7 @@ public class CaseGraphique extends JButton{
     @Override
     public void paintComponent (Graphics G){
         super.paintComponent(G);
-
+        
     }
     
     
